21d465dc7ab16f8110fec432570460d57e8de8f6
shiro <159876017+sukikeki0306@users.noreply.github.com>
2026-09-07 12:41:45 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+/* 一度開いたあとは通信がなくてもアプリを起動できるようにする。
+   index.html と同じ場所に置いてください。 */
+
+var CACHE = 'vocab-climb-v1';
+var FILES = ['./', './index.html'];
+
+self.addEventListener('install', function(e){
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); }).catch(function(){})
+  );
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.map(function(k){
+        return k === CACHE ? null : caches.delete(k);
+      }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(e){
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(function(res){
+      var copy = res.clone();
+      caches.open(CACHE).then(function(c){ c.put(e.request, copy); }).catch(function(){});
+      return res;
+    }).catch(function(){
+      return caches.match(e.request).then(function(hit){
+        return hit || caches.match('./index.html');
+      });
+    })
+  );
+});
